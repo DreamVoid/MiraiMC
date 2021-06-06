1efe79797fbc41e6a8b4397103b4bfed43e55729
流光溢彩Last <45266046+DreamVoid@users.noreply.github.com>
2021-06-07 00:30:44 +0800

    添加新的命令并修复机器人不存在时报错的问题

--- src/main/java/me/dreamvoid/miraimc/bukkit/CommandHandler.java
+++ src/main/java/me/dreamvoid/miraimc/bukkit/CommandHandler.java
@@ -2,6 +2,7 @@ package me.dreamvoid.miraimc.bukkit;
 
 import me.dreamvoid.miraimc.api.MiraiBot;
 import me.dreamvoid.miraimc.internal.Config;
+import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.utils.BotConfiguration;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -12,6 +13,8 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public class CommandHandler implements CommandExecutor {
 
     private final BukkitPlugin plugin;
@@ -19,7 +22,7 @@ public class CommandHandler implements CommandExecutor {
 
     public CommandHandler(BukkitPlugin plugin) {
         this.plugin = plugin;
-        this.mirai = new MiraiBot();
+        this.mirai = new MiraiBot(plugin.getLogger());
     }
 
     @Override
@@ -72,20 +75,57 @@ public class CommandHandler implements CommandExecutor {
                         }
                         return true;
                     }
+                    case "sendgroupnudge":{
+                        if(args.length>=4){
+                            mirai.sendGroupNudge(Long.parseLong(args[1]), Long.parseLong(args[2]));
+                        } else {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&c无效的参数！用法: /mirai sendgroupnudge <账号> <群号>"));
+                        }
+                        return true;
+                    }
                     case "sendfriendmessage":{
                         if(args.length>=4){
                             mirai.sendFriendMessage(Long.parseLong(args[1]), Long.parseLong(args[2]),args[3]);
                         } else {
-                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&c无效的参数！用法: /mirai sendfriendmessage <账号> <好友账号> <消息>"));
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&c无效的参数！用法: /mirai sendfriendmessage <账号> <好友> <消息>"));
                         }
+                        return true;
+                    }
+                    case "sendfriendnudge":{
+                        if(args.length>=4){
+                            mirai.sendFriendNudge(Long.parseLong(args[1]), Long.parseLong(args[2]));
+                        } else {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&c无效的参数！用法: /mirai sendfriendnudge <账号> <好友>"));
+                        }
+                        return true;
+                    }
+                    case "list":{
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&a当前在线的机器人: "));
+                        List<Long> BotList = mirai.getOnlineBots();
+                        for (Long bots : BotList){
+                            Bot bot=Bot.getInstance(bots);
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&b"+bot.getId() + "&r &7-&r &6"+Bot.getInstance(bots).getNick()));
+                        }
+                        return true;
+                    }
+                    case "checkonline":{
+                        if(args.length>=2){
+                            if(mirai.isBotOnline(Long.parseLong(args[1]))){
+                                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&a当前机器人在线"));
+                            } else sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&e当前机器人不在线"));
+                        } else sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&c无效的参数！用法: /mirai checkonline <账号>"));
                         return true;
                     }
                     case "help":{
                         sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&6&lMiraiMC&r &b机器人帮助菜单"));
                         sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&6/mirai login <账号> <密码> [协议]:&r 登录一个机器人"));
                         sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&6/mirai logout <账号>:&r 关闭一个机器人线程"));
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&6/mirai sendfriendmessage <账号> <好友账号> <消息>:&r 向指定好友发送私聊消息"));
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&6/mirai list:&r 查看当前在线的机器人"));
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&6/mirai sendfriendmessage <账号> <好友> <消息>:&r 向指定好友发送私聊消息"));
                         sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&6/mirai sendgroupmessage <账号> <群号> <消息>:&r 向指定群发送群聊消息"));
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&6/mirai sendfriendnudge <账号> <好友>:&r 向指定好友发送戳一戳"));
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&6/mirai sendgroupnudge <账号> <群号>:&r 向指定群发送戳一戳"));
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&6/mirai checkonline <账号>:&r 检查指定的机器人是否在线"));
                         return true;
                     }
                     default:{
