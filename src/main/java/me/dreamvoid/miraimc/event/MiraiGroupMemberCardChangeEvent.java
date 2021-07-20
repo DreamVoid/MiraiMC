e44862021a9cdb1efcbee566d7691dce57cd7728
流光溢彩Last <dreamvoid@dreamvoid.ml>
2021-07-20 21:02:31 +0800

    添加所有剩余的Mirai事件
    群名改变: MiraiGroupNameChangeEvent
    入群公告改变: MiraiGroupEntranceAnnouncementChangeEvent
    全员禁言状态改变: MiraiGroupMuteAllEvent
    匿名聊天状态改变: MiraiGroupAllowAnonymousChatEvent
    允许群员邀请好友加群状态改变: MiraiGroupAllowMemberInviteEvent
    成员群名片改动: MiraiGroupMemberCardChangeEvent
    成员群头衔改动: MiraiGroupMemberSpecialTitleChangeEvent
    成员权限改变: MiraiGroupMemberPermissionChangeEvent
    群成员被禁言: MiraiGroupMemberMuteEvent
    群成员被取消禁言: MiraiGroupMemberUnmuteEvent

--- src/main/java/me/dreamvoid/miraimc/event/MiraiGroupMemberCardChangeEvent.java
+++ src/main/java/me/dreamvoid/miraimc/event/MiraiGroupMemberCardChangeEvent.java
@@ -1,6 +1,6 @@
-package MiraiMC;
+package me.dreamvoid.miraimc.event;
 
-import net.mamoe.mirai.event.events.BotOnlineEvent;
+import net.mamoe.mirai.event.events.MemberCardChangeEvent;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
@@ -11,14 +11,14 @@ import org.jetbrains.annotations.NotNull;
  * 所有Mirai事件均有方法getBot，因此使用子方法getId作为基础方法
  * @author DreamVoid
  */
-public class EventTemplate extends Event {
-    public EventTemplate(BotOnlineEvent event) {
+public class MiraiGroupMemberCardChangeEvent extends Event {
+    public MiraiGroupMemberCardChangeEvent(MemberCardChangeEvent event) {
         super(true);
         this.event = event;
     }
 
     private static final HandlerList handlers = new HandlerList();
-    private final BotOnlineEvent event;
+    private final MemberCardChangeEvent event;
 
     public @NotNull HandlerList getHandlers() { return handlers; }
     public static HandlerList getHandlerList() { return handlers; }
@@ -30,10 +30,34 @@ public class EventTemplate extends Event {
     public long getBotID() { return event.getBot().getId(); }
 
     /**
+     * 获取群成员更改之前的名片
+     * @return 群名片
+     */
+    public String getOldNick() {
+        return event.getOrigin();
+    }
+
+    /**
+     * 获取群成员更改之后的名片
+     * @return 群名片
+     */
+    public String getNewNick() {
+        return event.getNew();
+    }
+
+    /**
      * 获取哈希值
      * @return 哈希值
      */
     public int getHashCode() {
         return event.hashCode();
+    }
+
+    /**
+     * 获取群成员QQ号
+     * @return QQ号
+     */
+    public long getMemberID(){
+        return event.getMember().getId();
     }
 }
--- src/main/java/me/dreamvoid/miraimc/event/MiraiGroupMuteAllEvent.java
+++ src/main/java/me/dreamvoid/miraimc/event/MiraiGroupMuteAllEvent.java
@@ -1,6 +1,6 @@
-package MiraiMC;
+package me.dreamvoid.miraimc.event;
 
-import net.mamoe.mirai.event.events.BotOnlineEvent;
+import net.mamoe.mirai.event.events.GroupMuteAllEvent;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
@@ -11,14 +11,14 @@ import org.jetbrains.annotations.NotNull;
  * 所有Mirai事件均有方法getBot，因此使用子方法getId作为基础方法
  * @author DreamVoid
  */
-public class EventTemplate extends Event {
-    public EventTemplate(BotOnlineEvent event) {
+public class MiraiGroupMuteAllEvent extends Event {
+    public MiraiGroupMuteAllEvent(GroupMuteAllEvent event) {
         super(true);
         this.event = event;
     }
 
     private static final HandlerList handlers = new HandlerList();
-    private final BotOnlineEvent event;
+    private final GroupMuteAllEvent event;
 
     public @NotNull HandlerList getHandlers() { return handlers; }
     public static HandlerList getHandlerList() { return handlers; }
@@ -35,5 +35,32 @@ public class EventTemplate extends Event {
      */
     public int getHashCode() {
         return event.hashCode();
+    }
+
+    /**
+     * 获取群号
+     * @return 群号
+     */
+    public long getGroupID(){
+        return event.getGroupId();
+    }
+
+    /**
+     * 获取操作管理员QQ号
+     * 如果不存在，返回0
+     * @return QQ号
+     */
+    public long getOperatorID(){
+        if(event.getOperator()!=null){
+            return event.getOperator().getId();
+        } else return 0L;
+    }
+
+    /**
+     * 获取群当前是否允许聊天
+     * @return 是否允许聊天
+     */
+    public boolean isAllowChat(){
+        return event.getNew();
     }
 }
