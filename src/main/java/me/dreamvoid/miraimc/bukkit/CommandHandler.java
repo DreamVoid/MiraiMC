69d569840dcc7ef08370993512de63e803065969
流光溢彩Last <dreamvoid@dreamvoid.ml>
2021-07-21 23:26:09 +0800

    初步重构登录验证

--- src/main/java/me/dreamvoid/miraimc/bukkit/CommandHandler.java
+++ src/main/java/me/dreamvoid/miraimc/bukkit/CommandHandler.java
@@ -232,10 +232,49 @@ public class CommandHandler implements CommandExecutor {
                 }
             }
             case "miraiverify":{
-                if(args[0].equalsIgnoreCase("unsafedevice") && args.length>=2){
-                    MiraiLoginSolver.solveUnsafeDeviceLoginVerify(Long.parseLong(args[1]),false);
-                }else if(args[0].equalsIgnoreCase("unsafedevicecancel") && args.length>=2){
-                    MiraiLoginSolver.solveUnsafeDeviceLoginVerify(Long.parseLong(args[1]),true);
+                switch (args[0].toLowerCase()){
+                    case "unsafedevice":{
+                        if(args.length >= 2){
+                            MiraiLoginSolver.solveUnsafeDeviceLoginVerify(Long.parseLong(args[1]),false);
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&a已将验证请求提交到服务器"));
+                        } else sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&c无效的参数！用法：/miraiverify unsafedevice <账号>"));
+                        break;
+                    }
+                    case "unsafedevicecancel":{
+                        if(args.length >= 2){
+                            MiraiLoginSolver.solveUnsafeDeviceLoginVerify(Long.parseLong(args[1]),true);
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&a已取消登录验证流程"));
+                        } else sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&c无效的参数！用法：/miraiverify unsafedevicecancel <账号>"));
+                        break;
+                    }
+                    case "slidercaptcha":{
+                        if(args.length >= 3){
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&a已将ticket提交到服务器"));
+                            MiraiLoginSolver.solveSliderCaptcha(Long.parseLong(args[1]),args[2]);
+                        } else sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&c无效的参数！用法：/miraiverify slidercaptcha <账号> <ticket>"));
+                        break;
+                    }
+                    case "slidercaptchacancel":{
+                        if(args.length >= 2){
+                            MiraiLoginSolver.solveSliderCaptcha(Long.parseLong(args[1]),true);
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&a已取消登录验证流程"));
+                        } else sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&c无效的参数！用法：/miraiverify slidercaptchacancel <账号>"));
+                        break;
+                    }
+                    case "piccaptcha":{
+                        if(args.length >= 3){
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&a已将验证码提交到服务器"));
+                            MiraiLoginSolver.solveSliderCaptcha(Long.parseLong(args[1]),args[2]);
+                        } else sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&c无效的参数！用法：/miraiverify piccaptcha <账号> <ticket>"));
+                        break;
+                    }
+                    case "piccaptchacancel":{
+                        if(args.length >= 2){
+                            MiraiLoginSolver.solveSliderCaptcha(Long.parseLong(args[1]),true);
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&a已取消登录验证流程"));
+                        } else sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&c无效的参数！用法：/miraiverify piccaptchacancel <账号>"));
+                        break;
+                    }
                 }
                 return true;
             }
--- src/test/java/MiraiMC/LoginSolverTest/MyLoginSolver.java
+++ src/test/java/MiraiMC/LoginSolverTest/MyLoginSolver.java
@@ -3,30 +3,51 @@ package MiraiMC.LoginSolverTest;
 import kotlin.coroutines.Continuation;
 import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.utils.LoginSolver;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.io.*;
 
 public class MyLoginSolver extends LoginSolver {
 
     @Nullable
     @Override
-    public Object onSolvePicCaptcha(@NotNull Bot bot, @NotNull byte[] bytes, @NotNull Continuation<? super String> continuation) {
-        System.out.println("onSolvePicCaptcha");
-        System.out.println("QQ: " + bot.getId());
-        System.out.println("Bytes: " + bytes);
-        System.out.println("Continuation: " + continuation);
+    public String onSolvePicCaptcha(@NotNull Bot bot, @NotNull byte[] bytes, @NotNull Continuation<? super String> continuation) {
+        bot.getLogger().verbose("onSolvePicCaptcha");
+        bot.getLogger().verbose("QQ: " + bot.getId());
+        bot.getLogger().verbose("Bytes: " + bytes);
+        bot.getLogger().verbose("Continuation: " + continuation);
 
+        // 验证码保存到本地
+        File file = new File(Login.BaseDir,"verify.png");
+        try (OutputStream fos = new FileOutputStream(file)) {
+            fos.write(bytes);
+            fos.flush();
+            bot.getLogger().verbose("验证码图片文件已保存到: "+file.getPath());
+        } catch (IOException e) { e.printStackTrace(); }
+
+        // 先睡六秒钟
+        try { Thread.sleep(6000); } catch (InterruptedException e) { e.printStackTrace(); }
+
+        // 读取验证码
+        String str;
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(new File(Login.BaseDir,"verify.txt")));
+            str = in.readLine();
+            bot.getLogger().verbose("向服务器发送验证码: "+str);
+            return str;
+        } catch (IOException e) { e.printStackTrace(); }
+        bot.getLogger().verbose("向服务器发送null");
         return null;
     }
 
     @Nullable
     @Override
     public Object onSolveSliderCaptcha(@NotNull Bot bot, @NotNull String s, @NotNull Continuation<? super String> continuation) {
-        System.out.println("onSolveSliderCaptcha");
-        System.out.println("QQ: " + bot.getId());
-        System.out.println("String: " + s);
-        System.out.println("Continuation: " + continuation);
+        bot.getLogger().verbose("onSolveSliderCaptcha");
+        bot.getLogger().verbose("QQ: " + bot.getId());
+        bot.getLogger().verbose("String: " + s);
+        bot.getLogger().verbose("Continuation: " + continuation);
 
         return null;
     }
@@ -37,15 +58,15 @@ public class MyLoginSolver extends LoginSolver {
         Runnable thread = new Runnable() {
             @Override
             public void run() {
-                    System.out.println("onSolveUnsafeDeviceLoginVerify");
-                    System.out.println("QQ: " + bot.getId());
-                    System.out.println("String: " + s);
-                    System.out.println("Continuation: " + continuation);
-                    try {
-                        wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                bot.getLogger().verbose("onSolveUnsafeDeviceLoginVerify");
+                bot.getLogger().verbose("QQ: " + bot.getId());
+                bot.getLogger().verbose("String: " + s);
+                bot.getLogger().verbose("Continuation: " + continuation);
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         };
         thread.notify();
