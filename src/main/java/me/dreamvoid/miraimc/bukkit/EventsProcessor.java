856ea9a31972b384d2a42a2602f1510f351799d9
DreamVoid <dreamvoid@dreamvoid.ml>
2021-09-05 00:57:35 +0800

    增加Event: GroupRecall日志

--- src/main/java/me/dreamvoid/miraimc/bukkit/EventsProcessor.java
+++ src/main/java/me/dreamvoid/miraimc/bukkit/EventsProcessor.java
@@ -1,10 +1,13 @@
 package me.dreamvoid.miraimc.bukkit;
 
+import me.dreamvoid.miraimc.bukkit.event.MiraiGroupMessageRecallEvent;
 import me.dreamvoid.miraimc.internal.Utils;
 import me.dreamvoid.miraimc.bukkit.event.MiraiFriendMessageEvent;
 import me.dreamvoid.miraimc.bukkit.event.MiraiGroupMessageEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.Arrays;
 
 public class EventsProcessor implements Listener {
     @EventHandler
@@ -17,4 +20,8 @@ public class EventsProcessor implements Listener {
         Utils.logger.info("[FriendMessage/"+e.getBotID()+"] "+e.getSenderNick()+"("+e.getSenderID()+") -> "+e.getMessage());
     }
 
+    @EventHandler
+    public void onMiraiGroupRecall(MiraiGroupMessageRecallEvent e){
+        Utils.logger.info("[Event: GroupRecall/"+e.getBotID()+"] authorId="+e.getSenderID()+", messageIds="+ Arrays.toString(e.getMessageIds()) + ", messageTime="+e.getMessageTime()+", operator="+e.getOperatorID()+", group="+e.getGroupID());
+    }
 }
