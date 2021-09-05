907b209fa9fe6a75c40876f757132e668610a8f9
DreamVoid <i@dreamvoid.me>
2021-09-05 14:02:58 +0800

    移除不必要的事件监听

--- src/main/java/me/dreamvoid/miraimc/bukkit/EventsProcessor.java
+++ src/main/java/me/dreamvoid/miraimc/bukkit/EventsProcessor.java
@@ -1,10 +1,7 @@
 package me.dreamvoid.miraimc.bukkit;
 
-import me.dreamvoid.miraimc.bukkit.event.MiraiGroupBotInvitedJoinGroupRequestEvent;
-import me.dreamvoid.miraimc.bukkit.event.MiraiGroupMessageRecallEvent;
+import me.dreamvoid.miraimc.bukkit.event.*;
 import me.dreamvoid.miraimc.internal.Utils;
-import me.dreamvoid.miraimc.bukkit.event.MiraiFriendMessageEvent;
-import me.dreamvoid.miraimc.bukkit.event.MiraiGroupMessageEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -17,15 +14,5 @@ public class EventsProcessor implements Listener {
     @EventHandler
     public void onMiraiFriendMessageReceived(MiraiFriendMessageEvent e){
         Utils.logger.info("[FriendMessage/"+e.getBotID()+"] "+e.getSenderNick()+"("+e.getSenderID()+") -> "+e.getMessage());
-    }
-
-    @EventHandler
-    public void onMiraiGroupRecall(MiraiGroupMessageRecallEvent e){
-        Utils.logger.info("[Event/"+e.getBotID()+"] "+e.eventToString());
-    }
-
-    @EventHandler
-    public void onMiraiBotJoinGroupInviteEvent(MiraiGroupBotInvitedJoinGroupRequestEvent e){
-        Utils.logger.info("[Event/"+e.getBotID()+"] "+e.eventToString());
     }
 }
