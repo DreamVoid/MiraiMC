4cfc9b422da19314b02f49c760dab2c6ffdbddb9
DreamVoid <i@dreamvoid.me>
2021-09-25 15:55:13 +0800

    把```getMessage()```变成```getMessageContent()```，并新增```getMessageToString```，因为后者几乎没用

--- MiraiMC-Bukkit/src/main/java/me/dreamvoid/miraimc/bukkit/Events.java
+++ MiraiMC-Bukkit/src/main/java/me/dreamvoid/miraimc/bukkit/Events.java
@@ -8,11 +8,11 @@ import org.bukkit.event.Listener;
 public class Events implements Listener {
     @EventHandler
     public void onMiraiGroupMessageReceived(MiraiGroupMessageEvent e){
-        Utils.logger.info("[GroupMessage/"+e.getBotID()+"] ["+e.getGroupName()+"("+e.getGroupID()+")] "+e.getSenderNameCard()+"("+e.getSenderID()+") -> "+e.getMessage());
+        Utils.logger.info("[GroupMessage/"+e.getBotID()+"] ["+e.getGroupName()+"("+e.getGroupID()+")] "+e.getSenderNameCard()+"("+e.getSenderID()+") -> "+e.getMessageToMiraiCode());
     }
 
     @EventHandler
     public void onMiraiFriendMessageReceived(MiraiFriendMessageEvent e){
-        Utils.logger.info("[FriendMessage/"+e.getBotID()+"] "+e.getSenderNick()+"("+e.getSenderID()+") -> "+e.getMessage());
+        Utils.logger.info("[FriendMessage/"+e.getBotID()+"] "+e.getSenderNick()+"("+e.getSenderID()+") -> "+e.getMessageToMiraiCode());
     }
 }
--- MiraiMC-Bungee/src/main/java/me/dreamvoid/miraimc/bungee/event/MiraiFriendMessageEvent.java
+++ MiraiMC-Bungee/src/main/java/me/dreamvoid/miraimc/bungee/event/MiraiFriendMessageEvent.java
@@ -39,15 +39,14 @@ public final class MiraiFriendMessageEvent extends Event {
     }
 
     /**
-     * 返回接收到的消息内容<br>
-     * 此方法使用 toString()<br>
-     * Java 对象的 toString()，会尽可能包含多的信息用于调试作用，行为可能不确定<br>
-     * 如需处理常规消息内容，请使用 {@link #getMessageContent()}
-     * @return 原始消息内容
-     * @see #getMessageContent()  
+     * 返回接收到的消息内容转换到字符串的结果<br>
+     * 此方法使用 contentToString()<br>
+     * QQ 对话框中以纯文本方式会显示的消息内容，这适用于MC与QQ的消息互通等不方便展示原始内容的场景。<br>
+     * 无法用纯文字表示的消息会丢失信息，如任何图片都是 [图片]
+     * @return 转换字符串后的消息内容
      */
     public String getMessage(){
-        return event.getMessage().toString();
+        return event.getMessage().contentToString();
     }
 
     /**
@@ -56,9 +55,23 @@ public final class MiraiFriendMessageEvent extends Event {
      * QQ 对话框中以纯文本方式会显示的消息内容，这适用于MC与QQ的消息互通等不方便展示原始内容的场景。<br>
      * 无法用纯文字表示的消息会丢失信息，如任何图片都是 [图片]
      * @return 转换字符串后的消息内容
+     * @see #getMessage() 
+     * @deprecated 
      */
+    @Deprecated
     public String getMessageContent(){
         return event.getMessage().contentToString();
+    }
+
+    /**
+     * 返回接收到的消息内容<br>
+     * 此方法使用 toString()<br>
+     * Java 对象的 toString()，会尽可能包含多的信息用于调试作用，行为可能不确定<br>
+     * 如需处理常规消息内容，请使用 {@link #getMessageContent()}
+     * @return 原始消息内容
+     */
+    public String getMessageToString(){
+        return event.getMessage().toString();
     }
 
     /**
