2595c41a0845536e4fc2b16d4e822520e9fbeac7
DreamVoid <i@dreamvoid.me>
2022-01-25 00:27:45 +0800

    所有图片上传成功事件增加```isEmoji```方法
    #85

--- MiraiMC-Sponge/src/main/java/me/dreamvoid/miraimc/sponge/event/MiraiImageUploadSucceedEvent.java
+++ MiraiMC-Sponge/src/main/java/me/dreamvoid/miraimc/sponge/event/MiraiImageUploadSucceedEvent.java
@@ -79,4 +79,12 @@ public class MiraiImageUploadSucceedEvent extends AbstractEvent {
     public @NotNull Cause getCause() {
         return cause;
     }
+
+    /**
+     * 判断图片是否为表情
+     * @return 是则返回true，不是返回false
+     */
+    public boolean isEmoji(){
+        return event.getImage().isEmoji();
+    }
 }
