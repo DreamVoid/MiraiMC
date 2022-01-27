7e46304c465726d7a94a0f9c671e359d3b3a3a55
DreamVoid <i@dreamvoid.me>
2022-01-28 01:28:41 +0800

    [ci skip] Avoid using finalizer method.

--- MiraiMC-Base/src/main/java/me/dreamvoid/miraimc/internal/httpapi/exception/AbnormalStatusException.java
+++ MiraiMC-Base/src/main/java/me/dreamvoid/miraimc/internal/httpapi/exception/AbnormalStatusException.java
@@ -1,8 +1,5 @@
 package me.dreamvoid.miraimc.internal.httpapi.exception;
 
-import java.io.PrintStream;
-import java.io.PrintWriter;
-
 public class AbnormalStatusException extends Exception{
     private final int code;
 
@@ -38,75 +35,5 @@ public class AbnormalStatusException extends Exception{
     @Override
     public String getMessage() {
         return super.getMessage();
-    }
-
-    @Override
-    public String getLocalizedMessage() {
-        return super.getLocalizedMessage();
-    }
-
-    @Override
-    public synchronized Throwable getCause() {
-        return super.getCause();
-    }
-
-    @Override
-    public synchronized Throwable initCause(Throwable cause) {
-        return super.initCause(cause);
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-
-    @Override
-    public void printStackTrace() {
-        super.printStackTrace();
-    }
-
-    @Override
-    public void printStackTrace(PrintStream s) {
-        super.printStackTrace(s);
-    }
-
-    @Override
-    public void printStackTrace(PrintWriter s) {
-        super.printStackTrace(s);
-    }
-
-    @Override
-    public synchronized Throwable fillInStackTrace() {
-        return super.fillInStackTrace();
-    }
-
-    @Override
-    public StackTraceElement[] getStackTrace() {
-        return super.getStackTrace();
-    }
-
-    @Override
-    public void setStackTrace(StackTraceElement[] stackTrace) {
-        super.setStackTrace(stackTrace);
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-
-    @Override
-    protected void finalize() throws Throwable {
-        super.finalize();
     }
 }
