433cdf2ebad40ff19d628943a5acf6a497f587e1
DreamVoid <i@dreamvoid.me>
2022-06-27 23:56:59 +0800

    调整插件检查更新

--- MiraiMC-Base/src/main/java/me/dreamvoid/miraimc/internal/PluginUpdate.java
+++ MiraiMC-Base/src/main/java/me/dreamvoid/miraimc/internal/PluginUpdate.java
@@ -1,15 +1,8 @@
 package me.dreamvoid.miraimc.internal;
 
-import com.google.gson.Gson;
 import me.dreamvoid.miraimc.webapi.Version;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 public class PluginUpdate {
     private final String latestRelease;
@@ -19,28 +12,7 @@ public class PluginUpdate {
     private final Version version;
 
     public PluginUpdate() throws IOException {
-        URL url = new URL("https://api.miraimc.dreamvoid.me/version.json");
-        StringBuilder sb = new StringBuilder();
-        HttpURLConnection httpUrlConn = (HttpURLConnection) url.openConnection();
-
-        httpUrlConn.setDoInput(true);
-        httpUrlConn.setRequestMethod("GET");
-        httpUrlConn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko");
-
-        InputStream input = httpUrlConn.getInputStream();
-        InputStreamReader read = new InputStreamReader(input, StandardCharsets.UTF_8);
-        BufferedReader br = new BufferedReader(read);
-        String data = br.readLine();
-        while (data != null) {
-            sb.append(data);
-            data = br.readLine();
-        }
-        br.close();
-        read.close();
-        input.close();
-        httpUrlConn.disconnect();
-
-        version = new Gson().fromJson(sb.toString(), Version.class);
+        version = Version.init();
 
         latestRelease = version.latest;
         latestReleaseNo = version.versions.get(version.latest);
