35d276d8af41512148d6e5b863e43b399f7ec180
DreamVoid <i@dreamvoid.me>
2022-06-30 01:48:23 +0800

    增加MiraiOtherClient类及相关方法

---
+++ MiraiMC-Base/src/main/java/me/dreamvoid/miraimc/api/bot/MiraiOtherClient.java
@@ -0,0 +1,61 @@
+package me.dreamvoid.miraimc.api.bot;
+
+import net.mamoe.mirai.contact.OtherClient;
+import net.mamoe.mirai.message.code.MiraiCode;
+
+/**
+ * MiraiMC 其他客户端
+ * @since 1.7
+ */
+public class MiraiOtherClient {
+	private final OtherClient client;
+
+	/**
+	 * 获取指定的其他客户端实例
+	 * @param OtherClient 其他客户端
+	 */
+	public MiraiOtherClient(OtherClient OtherClient) {
+		this.client = OtherClient;
+	}
+
+	/**
+	 * 向其他客户端发送消息
+	 * @param message 消息文本
+	 */
+	public void sendMessage(String message){
+		client.sendMessage(message);
+	}
+
+	/**
+	 * 向其他客户端发送消息<br>
+	 * 此方法将自动转换为Mirai Code，可用于发送图片等特殊消息
+	 * @param message Mirai Code格式的消息文本
+	 */
+	public void sendMessageMirai(String message){
+		client.sendMessage(MiraiCode.deserializeMiraiCode(message));
+	}
+
+	/**
+	 * 获取其他客户端ID
+	 * @return 客户端ID
+	 */
+	public long getID(){
+		return client.getId();
+	}
+
+	/**
+	 * 获取其他客户端名称
+	 * @return 客户端名称
+	 */
+	public String getDeviceName(){
+		return client.getInfo().getDeviceName();
+	}
+
+	/**
+	 * 获取其他客户端类型
+	 * @return 客户端类型
+	 */
+	public String getDeviceKind(){
+		return client.getInfo().getDeviceKind();
+	}
+}
