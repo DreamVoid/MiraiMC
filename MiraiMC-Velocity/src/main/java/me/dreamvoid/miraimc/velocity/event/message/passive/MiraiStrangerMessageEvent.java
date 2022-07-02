9ff2bfd0c517a06e6f50278fe52d276d93b7229a
流光溢彩Last <dreamvoid@dreamvoid.ml>
2022-07-03 03:27:28 +0800

    [Velocity] 添加HTTPAPI支持

--- MiraiMC-Velocity/src/main/java/me/dreamvoid/miraimc/velocity/event/message/passive/MiraiStrangerMessageEvent.java
+++ MiraiMC-Velocity/src/main/java/me/dreamvoid/miraimc/velocity/event/message/passive/MiraiStrangerMessageEvent.java
@@ -1,13 +1,17 @@
 package me.dreamvoid.miraimc.velocity.event.message.passive;
 
+import me.dreamvoid.miraimc.httpapi.response.FetchMessage;
 import net.mamoe.mirai.event.events.StrangerMessageEvent;
 
 /**
  * (Velocity) Mirai 核心事件 - 消息 - 被动收到消息 - 陌生人消息
  */
 public class MiraiStrangerMessageEvent extends AbstractMessageEvent {
-
     public MiraiStrangerMessageEvent(StrangerMessageEvent event) {
         super(event);
+    }
+
+    public MiraiStrangerMessageEvent(long BotID, FetchMessage.Data data) {
+        super(BotID, data);
     }
 }
