96ec437091aad8cc2228160fda0a4b4ab47aefe7
流光溢彩Last <dreamvoid@dreamvoid.ml>
2022-07-03 16:50:41 +0800

    Release 1.7-rc1

--- MiraiMC-Sponge/src/main/java/me/dreamvoid/miraimc/sponge/SpongePlugin.java
+++ MiraiMC-Sponge/src/main/java/me/dreamvoid/miraimc/sponge/SpongePlugin.java
@@ -32,7 +32,7 @@ import java.util.concurrent.TimeUnit;
 @Plugin(id = "miraimc",
         name = "MiraiMC",
         description = "MiraiBot for Minecraft server",
-        version = "1.7-pre3",
+        version = "1.7-rc1",
         url = "https://github.com/DreamVoid/MiraiMC",
         authors = {"DreamVoid"}
 )
