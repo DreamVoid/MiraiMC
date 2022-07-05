597015a0a48b62010d4f75dcbcb64315686738b8
DreamVoid <i@dreamvoid.me>
2022-07-05 14:42:48 +0800

    将API地址内置到jar中

--- MiraiMC-Base/src/main/java/me/dreamvoid/miraimc/webapi/Version.java
+++ MiraiMC-Base/src/main/java/me/dreamvoid/miraimc/webapi/Version.java
@@ -2,14 +2,10 @@ package me.dreamvoid.miraimc.webapi;
 
 import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
-import me.dreamvoid.miraimc.internal.Config;
 import me.dreamvoid.miraimc.internal.Utils;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -31,16 +27,6 @@ public final class Version {
 	public static Version init() throws IOException {
 		if (INSTANCE == null) {
 			List<String> list = new ArrayList<>(Info.init().apis);
-
-			try {
-				File cache = new File(Config.PluginDir, "apis.json");
-				if (cache.exists()) {
-					String[] localString = new Gson().fromJson(new FileReader(cache), String[].class);
-					List<String> local = new ArrayList<>(Arrays.asList(localString)); // 这里不双重调用下面的removeAll就不能用
-					local.removeAll(list);
-					list.addAll(local);
-				}
-			} catch (Exception ignored) {}
 
 			IOException e = null; // 用于所有API都炸掉的时候抛出的
 
