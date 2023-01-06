d88d4dc2f433e62e22eb7ab2e54ecd3cd35e0d3b
DreamVoid <i@dreamvoid.me>
2023-01-06 22:32:02 +0800

    改个变量名

--- MiraiMC-Bungee/src/main/java/me/dreamvoid/miraimc/bungee/BungeeConfig.java
+++ MiraiMC-Bungee/src/main/java/me/dreamvoid/miraimc/bungee/BungeeConfig.java
@@ -11,7 +11,7 @@ import java.nio.file.Files;
 import static me.dreamvoid.miraimc.internal.Config.*;
 
 public class BungeeConfig {
-    private Configuration bungeeConfig;
+    private Configuration config;
     private final BungeePlugin BungeePlugin;
     private static BungeeConfig Instance;
 
@@ -30,40 +30,40 @@ public class BungeeConfig {
                     Files.copy(in, file.toPath());
                 }
             }
-            bungeeConfig = ConfigurationProvider.getProvider(net.md_5.bungee.config.YamlConfiguration.class).load(new File(BungeePlugin.getDataFolder(), "config.yml"));
+            config = ConfigurationProvider.getProvider(net.md_5.bungee.config.YamlConfiguration.class).load(new File(BungeePlugin.getDataFolder(), "config.yml"));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        General.AllowBStats = bungeeConfig.getBoolean("general.allow-bStats",true);
-        General.CheckUpdate = bungeeConfig.getBoolean("general.check-update",true);
-        General.DisableSafeWarningMessage = bungeeConfig.getBoolean("general.disable-safe-warning-message",false);
-        General.MiraiWorkingDir = bungeeConfig.getString("general.mirai-working-dir","default");
-        General.MiraiCoreVersion = bungeeConfig.getString("general.mirai-core-version","latest");
-        General.MavenRepoUrl = bungeeConfig.getString("general.maven-repo-url","https://repo1.maven.org/maven2");
-        General.EnableHttpApi = bungeeConfig.getBoolean("general.enable-http-api",false);
-        General.LegacyEventSupport = bungeeConfig.getBoolean("general.legacy-event-support",false);
+        General.AllowBStats = config.getBoolean("general.allow-bStats",true);
+        General.CheckUpdate = config.getBoolean("general.check-update",true);
+        General.DisableSafeWarningMessage = config.getBoolean("general.disable-safe-warning-message",false);
+        General.MiraiWorkingDir = config.getString("general.mirai-working-dir","default");
+        General.MiraiCoreVersion = config.getString("general.mirai-core-version","latest");
+        General.MavenRepoUrl = config.getString("general.maven-repo-url","https://repo1.maven.org/maven2");
+        General.EnableHttpApi = config.getBoolean("general.enable-http-api",false);
+        General.LegacyEventSupport = config.getBoolean("general.legacy-event-support",false);
 
-        Bot.DisableNetworkLogs = bungeeConfig.getBoolean("bot.disable-network-logs",false);
-        Bot.DisableBotLogs = bungeeConfig.getBoolean("bot.disable-bot-logs",false);
-        Bot.UseMinecraftLogger.BotLogs = bungeeConfig.getBoolean("bot.use-minecraft-logger.bot-logs",true);
-        Bot.UseMinecraftLogger.NetworkLogs = bungeeConfig.getBoolean("bot.use-minecraft-logger.network-logs",true);
-        Bot.LogEvents = bungeeConfig.getBoolean("bot.log-events",true);
-        Bot.ContactCache.EnableFriendListCache = bungeeConfig.getBoolean("bot.contact-cache.enable-friend-list-cache",false);
-        Bot.ContactCache.EnableGroupMemberListCache = bungeeConfig.getBoolean("bot.contact-cache.enable-group-member-list-cache",false);
-        Bot.ContactCache.SaveIntervalMillis = bungeeConfig.getLong("bot.contact-cache.save-interval-millis",60000);
+        Bot.DisableNetworkLogs = config.getBoolean("bot.disable-network-logs",false);
+        Bot.DisableBotLogs = config.getBoolean("bot.disable-bot-logs",false);
+        Bot.UseMinecraftLogger.BotLogs = config.getBoolean("bot.use-minecraft-logger.bot-logs",true);
+        Bot.UseMinecraftLogger.NetworkLogs = config.getBoolean("bot.use-minecraft-logger.network-logs",true);
+        Bot.LogEvents = config.getBoolean("bot.log-events",true);
+        Bot.ContactCache.EnableFriendListCache = config.getBoolean("bot.contact-cache.enable-friend-list-cache",false);
+        Bot.ContactCache.EnableGroupMemberListCache = config.getBoolean("bot.contact-cache.enable-group-member-list-cache",false);
+        Bot.ContactCache.SaveIntervalMillis = config.getLong("bot.contact-cache.save-interval-millis",60000);
 
-        Database.Type = bungeeConfig.getString("database.type","sqlite").toLowerCase();
-        Database.MySQL.Address = bungeeConfig.getString("database.mysql.address", "localhost");
-        Database.MySQL.Username = bungeeConfig.getString("database.mysql.username", "miraimc");
-        Database.MySQL.Password = bungeeConfig.getString("database.mysql.password", "miraimc");
-        Database.MySQL.Database = bungeeConfig.getString("database.mysql.database", "miraimc");
-        Database.MySQL.Poll.ConnectionTimeout = bungeeConfig.getInt("database.mysql.pool.connectionTimeout",30000);
-        Database.MySQL.Poll.IdleTimeout = bungeeConfig.getInt("database.mysql.pool.connectionTimeout",600000);
-        Database.MySQL.Poll.MaxLifetime = bungeeConfig.getInt("database.mysql.pool.maxLifetime",1800000);
-        Database.MySQL.Poll.MaximumPoolSize = bungeeConfig.getInt("database.mysql.pool.maximumPoolSize",15);
-        Database.MySQL.Poll.KeepaliveTime = bungeeConfig.getInt("database.mysql.pool.keepaliveTime",0);
-        Database.MySQL.Poll.MinimumIdle = bungeeConfig.getInt("database.mysql.pool.minimumIdle",0);
+        Database.Type = config.getString("database.type","sqlite").toLowerCase();
+        Database.MySQL.Address = config.getString("database.mysql.address", "localhost");
+        Database.MySQL.Username = config.getString("database.mysql.username", "miraimc");
+        Database.MySQL.Password = config.getString("database.mysql.password", "miraimc");
+        Database.MySQL.Database = config.getString("database.mysql.database", "miraimc");
+        Database.MySQL.Poll.ConnectionTimeout = config.getInt("database.mysql.pool.connectionTimeout",30000);
+        Database.MySQL.Poll.IdleTimeout = config.getInt("database.mysql.pool.connectionTimeout",600000);
+        Database.MySQL.Poll.MaxLifetime = config.getInt("database.mysql.pool.maxLifetime",1800000);
+        Database.MySQL.Poll.MaximumPoolSize = config.getInt("database.mysql.pool.maximumPoolSize",15);
+        Database.MySQL.Poll.KeepaliveTime = config.getInt("database.mysql.pool.keepaliveTime",0);
+        Database.MySQL.Poll.MinimumIdle = config.getInt("database.mysql.pool.minimumIdle",0);
     }
 
     public static void reloadConfig() {
