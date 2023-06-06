71888c0b2481de5fc1c24e9f9aa23c6247dc1e41
流光溢彩Last <i@dreamvoid.me>
2023-06-06 20:06:00 +0800

    优化 /miraiverify 找不到QQ时的提示信息

--- MiraiMC-Base/src/main/java/me/dreamvoid/miraimc/commands/MiraiVerifyCommand.java
+++ MiraiMC-Base/src/main/java/me/dreamvoid/miraimc/commands/MiraiVerifyCommand.java
@@ -2,6 +2,8 @@ package me.dreamvoid.miraimc.commands;
 
 import me.dreamvoid.miraimc.internal.MiraiLoginSolver;
 
+import java.util.NoSuchElementException;
+
 public class MiraiVerifyCommand implements ICommandExecutor {
     @Override
     public boolean onCommand(ICommandSender sender, String[] args) {
@@ -10,40 +12,44 @@ public class MiraiVerifyCommand implements ICommandExecutor {
             return true;
         }
 
-        switch (args[0].toLowerCase()){
-            case "unsafedevice":{
-                if(args.length >= 2){
-                    MiraiLoginSolver.solve(Long.parseLong(args[1]));
-                    sender.sendMessage("&a已将验证请求提交到服务器");
-                } else sender.sendMessage("&c无效的参数！用法：/miraiverify unsafedevice <账号>");
-                break;
-            }
-            case "captcha":{
-                if(args.length >= 3){
-                    sender.sendMessage("&a已将验证码提交到服务器");
-                    MiraiLoginSolver.solve(Long.parseLong(args[1]),args[2]);
-                } else sender.sendMessage("&c无效的参数！用法：/miraiverify captcha <账号> <验证码>");
-                break;
-            }
-            case "cancel":{
-                if(args.length >= 2){
-                    MiraiLoginSolver.cancel(Long.parseLong(args[1]));
-                    sender.sendMessage("&a已取消登录验证流程");
-                } else sender.sendMessage("&c无效的参数！用法：/miraiverify cancel <账号>");
-                break;
-            }
-            case "deviceverify":{
-                if(args.length >= 2){
-                    if(args.length == 2){
+        try {
+            switch (args[0].toLowerCase()) {
+                case "unsafedevice": {
+                    if (args.length >= 2) {
                         MiraiLoginSolver.solve(Long.parseLong(args[1]));
                         sender.sendMessage("&a已将验证请求提交到服务器");
-                    } else if(args.length == 3){
-                        MiraiLoginSolver.solve(Long.parseLong(args[1]), args[2]);
+                    } else sender.sendMessage("&c无效的参数！用法：/miraiverify unsafedevice <账号>");
+                    break;
+                }
+                case "captcha": {
+                    if (args.length >= 3) {
                         sender.sendMessage("&a已将验证码提交到服务器");
-                    }
-                } else sender.sendMessage("&c无效的参数！用法：/miraiverify unsafedevice <账号>");
-                break;
+                        MiraiLoginSolver.solve(Long.parseLong(args[1]), args[2]);
+                    } else sender.sendMessage("&c无效的参数！用法：/miraiverify captcha <账号> <验证码>");
+                    break;
+                }
+                case "cancel": {
+                    if (args.length >= 2) {
+                        MiraiLoginSolver.cancel(Long.parseLong(args[1]));
+                        sender.sendMessage("&a已取消登录验证流程");
+                    } else sender.sendMessage("&c无效的参数！用法：/miraiverify cancel <账号>");
+                    break;
+                }
+                case "deviceverify": {
+                    if (args.length >= 2) {
+                        if (args.length == 2) {
+                            MiraiLoginSolver.solve(Long.parseLong(args[1]));
+                            sender.sendMessage("&a已将验证请求提交到服务器");
+                        } else if (args.length == 3) {
+                            MiraiLoginSolver.solve(Long.parseLong(args[1]), args[2]);
+                            sender.sendMessage("&a已将验证码提交到服务器");
+                        }
+                    } else sender.sendMessage("&c无效的参数！用法：/miraiverify unsafedevice <账号>");
+                    break;
+                }
             }
+        } catch (NoSuchElementException e){
+            sender.sendMessage("&c找不到指定的机器人QQ！");
         }
         return true;
     }
