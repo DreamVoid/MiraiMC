4c01710e1ba6c7684f4dc363322711e6877dea9c
流光溢彩Last <i@dreamvoid.me>
2023-07-31 21:47:27 +0800

    优化Kotlin方法

--- MiraiMC-Base/src/main/kotlin/me/dreamvoid/miraimc/internal/MiraiEncryptServiceFactoryKt.kt
+++ MiraiMC-Base/src/main/kotlin/me/dreamvoid/miraimc/internal/MiraiEncryptServiceFactoryKt.kt
@@ -1,14 +1,10 @@
 package me.dreamvoid.miraimc.internal
 
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonNames
 import net.mamoe.mirai.internal.spi.EncryptService
 import net.mamoe.mirai.internal.spi.EncryptServiceContext
+import net.mamoe.mirai.internal.utils.*
 import net.mamoe.mirai.utils.BotConfiguration
 import net.mamoe.mirai.utils.Services
-import net.mamoe.mirai.internal.utils.*;
 
 class MiraiEncryptServiceFactoryKt {
     companion object{
@@ -18,32 +14,14 @@ class MiraiEncryptServiceFactoryKt {
         }
 
         @JvmStatic
+        fun getProtocol(context : EncryptServiceContext) : BotConfiguration.MiraiProtocol{
+            return context.extraArgs[EncryptServiceContext.KEY_BOT_PROTOCOL]
+        }
+
+        @JvmStatic
         @Suppress("INVISIBLE_MEMBER")
         fun getProtocolVersion(protocol : BotConfiguration.MiraiProtocol) : String {
             return MiraiProtocolInternal[protocol].ver
         }
-
-        @JvmStatic
-        fun getProtocol(context : EncryptServiceContext) : BotConfiguration.MiraiProtocol{
-            return context.extraArgs[EncryptServiceContext.KEY_BOT_PROTOCOL]
-        }
     }
-
-    @Serializable
-    @OptIn(ExperimentalSerializationApi::class)
-    private data class ServerConfig(
-        @SerialName("base_url")
-        val base: String,
-        @SerialName("type")
-        val type: String = "",
-        @SerialName("key")
-        val key: String = "",
-        @SerialName("server_identity_key")
-        @JsonNames("serverIdentityKey")
-        val serverIdentityKey: String = "",
-        @SerialName("authorization_key")
-        @JsonNames("authorizationKey")
-        val authorizationKey: String = ""
-    )
-
 }
