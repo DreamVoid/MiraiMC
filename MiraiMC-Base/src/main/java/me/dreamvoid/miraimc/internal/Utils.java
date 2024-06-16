3464db8e6a5e3d7bb02744f43d4dce76e1f70174
流光 <i@dreamvoid.me>
2024-06-16 21:50:52 +0800

    重写 HTTP 部分以缩减插件文件大小

--- MiraiMC-Base/src/main/java/me/dreamvoid/miraimc/internal/Utils.java
+++ MiraiMC-Base/src/main/java/me/dreamvoid/miraimc/internal/Utils.java
@@ -1,14 +1,8 @@
 package me.dreamvoid.miraimc.internal;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import me.dreamvoid.miraimc.internal.config.PluginConfig;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicHeader;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
@@ -110,61 +104,60 @@ public final class Utils {
          */
         public static String get(String url) throws IOException {
             URL obj = new URL(url.replace(" ", "%20"));
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+            connection.setDoInput(true);
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 DreamVoid MiraiMC");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(10000);
+
             StringBuilder sb = new StringBuilder();
-            HttpURLConnection httpUrlConn = (HttpURLConnection) obj.openConnection();
-
-            httpUrlConn.setDoInput(true);
-            httpUrlConn.setRequestMethod("GET");
-            httpUrlConn.setRequestProperty("User-Agent", "Mozilla/5.0 DreamVoid MiraiMC");
-            httpUrlConn.setConnectTimeout(5000);
-            httpUrlConn.setReadTimeout(10000);
-
-            InputStream input = httpUrlConn.getInputStream();
-            InputStreamReader read = new InputStreamReader(input, StandardCharsets.UTF_8);
-            BufferedReader br = new BufferedReader(read);
-            String data = br.readLine();
-            while (data != null) {
-                sb.append(data).append(System.lineSeparator());
-                data = br.readLine();
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
+                String data = br.readLine();
+                while (data != null) {
+                    sb.append(data).append(System.lineSeparator());
+                    data = br.readLine();
+                }
             }
-            br.close();
-            read.close();
-            input.close();
-            httpUrlConn.disconnect();
 
+            connection.disconnect();
             return sb.toString();
         }
 
         /**
          * 发送HTTP POST请求
          * @param json Gson对象
-         * @param URL 链接
+         * @param url 链接
          * @return 远程服务器返回内容
          */
-        public static String post(JsonObject json, String URL) throws IOException {
-            try (CloseableHttpClient client = HttpClients.createDefault()) {
-                HttpPost post = new HttpPost(URL);
-                post.setHeader("Content-Type", "application/json");
-                post.addHeader("Authorization", "Basic YWRtaW46");
-                StringEntity s = new StringEntity(json.toString(), StandardCharsets.UTF_8);
-                s.setContentType(new BasicHeader(org.apache.http.protocol.HTTP.CONTENT_TYPE, "application/json"));
-                post.setEntity(s);
-                // 发送请求
-                HttpResponse httpResponse = client.execute(post);
-                // 获取响应输入流
-                InputStream inStream = httpResponse.getEntity().getContent();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(
-                        inStream, StandardCharsets.UTF_8));
-                StringBuilder strber = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null)
-                    strber.append(line).append("\n");
-                inStream.close();
-                if (httpResponse.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
-                    logger.warning("Http request returned bad status code: " + httpResponse.getStatusLine().getStatusCode()+", reason: "+ httpResponse.getStatusLine().getReasonPhrase());
-                }
-                return strber.toString();
+        public static String post(JsonObject json, String url) throws IOException {
+            URL obj = new URL(url.replace(" ", "%20"));
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+            connection.setDoInput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 DreamVoid MiraiMC");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("Authorization", "Basic YWRtaW46");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(10000);
+
+            try(DataOutputStream os = new DataOutputStream(connection.getOutputStream())){
+                os.write(new Gson().toJson(json).getBytes(StandardCharsets.UTF_8));
             }
+
+            StringBuilder sb = new StringBuilder();
+
+            try(BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))){
+                String data = br.readLine();
+                while (data != null) {
+                    sb.append(data).append(System.lineSeparator());
+                    data = br.readLine();
+                }
+            }
+
+            return sb.toString();
         }
 
         /**
